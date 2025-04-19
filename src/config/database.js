5e659e36f7c6fb34cef6787bd04452b48cdf37bb
ritikmalviya40@gmail.com <ritikmalviya40@gmail.com>
2025-04-19 11:44:10 +0530

    last commit

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const express = require("express")
 
 const connectDB = async () => {
-    console.log(process.env.DB_CONNECTION_STRING)
 
     await mongoose.connect(process.env.DB_CONNECTION_STRING)
 
